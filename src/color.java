f132ba1b758fb0062a89a5c14fe843c2499ce4fc
Aviral  kumar <aviralkumar9084@gmail.com>
2021-12-10 11:32:10 +0530

    Initial commit

---
+++ src/color.java
@@ -0,0 +1,16 @@
+class aviral
+{
+    String color;
+    int age;
+}
+
+public class color {
+    public static void main(String[] args) {
+        aviral n = new aviral();
+        n.age= 18;
+        n.color = "black";
+        System.out.println(n.color +"  "+  n.age);
+
+    }
+
+}
